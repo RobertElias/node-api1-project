31ad7582a9e32589754f59cfe2af5877bb5aec66
Robert Elias <RobertElias@users.noreply.github.com>
2020-02-10 21:08:24 -0800

    POST request to /api/users

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// implement your API here
+const express = require('express');
+
+const Users = require('./data/db.js')
+
+const server = express()
+
+server.get('/', (req, res) =>{
+    res.json({hello: 'You are working on NODE-API1-PROJECT'})
+});
+
+//view list of users with GET /api/users
+server.get('/api/users', (req, res)=>{
+    // get the users from the database
+    Users.find().then(users => {
+        res.status(200).json(users);
+    }).catch(err =>{
+        console.log(err);
+        res.status(500).json({errorMessage: 'sorry about that'})
+    });
+    
+})
+
+const port = 5000;
+server.listen(port, () => console.log(`\n** Apis is on port: ${port} **\n`));
